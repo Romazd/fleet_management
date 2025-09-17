4ea55402c34f2cad50b1c41b4a91f15b66b590ef
Marco Sandoval <segundo.marco@gmail.com>
2025-09-17 08:58:44 -0600

    User model migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_17_142953) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_17_144553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_17_142953) do
     t.index ["status"], name: "index_maintenance_services_on_status"
     t.index ["vehicle_id", "status"], name: "index_maintenance_services_on_vehicle_id_and_status"
     t.index ["vehicle_id"], name: "index_maintenance_services_on_vehicle_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index "lower((email)::text)", name: "index_users_on_lower_email", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "vehicles", force: :cascade do |t|
